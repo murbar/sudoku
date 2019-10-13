63caea78a29e72cf18ee09ab4d00fdf09bd19d0b
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-12 23:11:45 -0400

    Set min-width, add copy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from 'components/Header';
-import GameGrid from 'components/GameGrid';
-import NumbersControl from 'components/NumbersControl';
-import ColumnLabels from 'components/ColumnLabels';
-import RowLabels from 'components/RowLabels';
+// import NumbersTouchControl from 'components/NumbersTouchControl';
+import GridContainer from 'components/GridContainer';
 import useDocumentTitle from 'hooks/useDocumentTitle';
 import useSudoku from 'hooks/useSudoku';
 
 const Styles = styled.div`
-  max-width: 66rem;
+  max-width: 64rem;
+  min-width: 35rem;
   margin: 0 auto;
   padding: 0 2rem 4rem;
 `;
@@ -38,16 +37,14 @@ export default function App() {
       <p>
         Difficulty: {gameState.difficulty}, Hints remaining: {gameState.hintsRemaining}
       </p>
-      <NumbersControl />
-      <ColumnLabels />
-      <RowLabels />
-      <GameGrid
-        cells={gameState.currentCells}
-        isPaused={gameState.isPaused}
-        handleCellChange={gameActions.setCell}
-        startingCellIndexes={gameState.startingCellIndexes}
-        invalidCellIndexes={gameState.invalidCellIndexes}
-      />
+      <p>
+        Select a grid cell to enter a value 1 through 9. Navigate the grid with arrow keys
+        or go to a cell directly by pressing a row key (<kbd>A</kbd> - <kbd>I</kbd>)
+        followed by a column key (<kbd>1</kbd> - <kbd>9</kbd>). Cells with starting values
+        are not editable and will be skipped.
+      </p>
+      {/* <NumbersTouchControl /> */}
+      <GridContainer {...{ gameState, gameActions }} />
       <button onClick={gameActions.getHint} disabled={gameState.hintsRemaining < 1}>
         Get Hint
       </button>
@@ -60,7 +57,9 @@ export default function App() {
       <button onClick={gameActions.clearCells}>Clear</button>
       {/* <h2>Time: 00:00</h2> */}
       {/* <h2>Mode: play/solve</h2> */}
-      <h2>Solving a Sudoku puzzle with recursive backtracking</h2>
+      <h2>
+        Solving a Sudoku puzzle with Norvig's constraint propagation and search algorithm
+      </h2>
     </Styles>
   );
 }
