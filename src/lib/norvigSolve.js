321e28fcd43129e7a86defe27c81656df82407a4
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-14 14:19:42 -0400

    Implement Norvig's algorithm

---
+++ src/lib/norvigSolve.js
@@ -0,0 +1,218 @@
+import { deserializeGridValuesString } from './helpers';
+
+const VALUES = '123456789';
+const ROWS = [...'ABCDEFGHI'];
+const COLS = [...VALUES];
+const CELLS = permuteItems(ROWS, COLS);
+const GROUP_LIST = buildGroupList();
+const GROUPS = buildGroups();
+const PEERS = buildPeers();
+
+const opCounts = {
+  assigns: 0,
+  eliminations: 0,
+  searches: 0
+};
+
+const isKnownValue = possibleValues => possibleValues.length === 1;
+
+const failedRecursively = result => result === false;
+
+const removeValue = (valuesString, value) => valuesString.replace(value, '');
+
+function resetOpCounts() {
+  opCounts.assigns = 0;
+  opCounts.eliminations = 0;
+  opCounts.searches = 0;
+}
+
+function permuteItems(A, B) {
+  var products = [];
+  for (const a of A) for (const b of B) products.push(a + b);
+  return products;
+}
+
+function buildGroupList() {
+  const list = [];
+  for (const c of COLS) list.push(permuteItems(ROWS, c));
+  for (const r of ROWS) list.push(permuteItems(r, COLS));
+  const rRows = ['ABC', 'DEF', 'GHI'];
+  const cCols = ['123', '456', '789'];
+  for (const rs of rRows) for (const cs of cCols) list.push(permuteItems(rs, cs));
+  return list;
+}
+
+function buildGroups() {
+  return CELLS.reduce((groups, cell) => {
+    groups[cell] = GROUP_LIST.filter(group => group.includes(cell));
+    return groups;
+  }, {});
+}
+
+function buildPeers() {
+  return CELLS.reduce((peers, cell) => {
+    peers[cell] = [...new Set(GROUPS[cell].flat().filter(c => c !== cell))];
+    return peers;
+  }, {});
+}
+
+function parseGridValues(valuesString) {
+  resetOpCounts();
+
+  const parsed = deserializeGridValuesString(valuesString);
+
+  if (parsed.length !== 81) {
+    throw new Error('Invalid grid values string');
+  }
+
+  return CELLS.reduce((values, cell, index) => {
+    values[cell] = parsed[index];
+    return values;
+  }, {});
+}
+
+function calcPossibleValues(valuesString) {
+  const initialKnownValues = parseGridValues(valuesString);
+
+  const values = CELLS.reduce((object, cell) => {
+    object[cell] = VALUES;
+    return object;
+  }, {});
+
+  for (const [cell, value] of Object.entries(initialKnownValues)) {
+    const v = value.toString();
+    // console.log(cell, v);
+    if (VALUES.includes(v) && !assignValues(values, cell, v)) {
+      return false;
+    }
+  }
+
+  return values;
+}
+
+function assignValues(possiblesMap, cell, value) {
+  // Eliminate all the other values (except d) from values[cell] and propagate
+  // Return values, except return false if a contradiction is detected
+  opCounts.assigns++;
+
+  const possibles = [...removeValue(possiblesMap[cell], value)];
+
+  const wereFailures = possibles
+    .map(v => eliminateRecursively(possiblesMap, cell, v))
+    .some(failedRecursively);
+
+  return wereFailures ? false : possiblesMap;
+}
+
+function propagateSingleValueConstraint(possiblesMap, cell) {
+  const possibles = possiblesMap[cell];
+  const noPossibleValue = possibles.length === 0;
+  const singlePossibleValue = possibles.length === 1;
+
+  if (noPossibleValue) {
+    return false;
+  } else if (singlePossibleValue) {
+    const wereFailures = PEERS[cell]
+      .map(peer => eliminateRecursively(possiblesMap, peer, possibles))
+      .some(failedRecursively);
+
+    if (wereFailures) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function propagateSingleLocationConstraint(possiblesMap, cell, value) {
+  for (const group of GROUPS[cell]) {
+    const places = group.filter(c => possiblesMap[c].includes(value));
+    const noPossibleLocation = !places.length;
+    const singlePossibleLocation = places.length === 1;
+
+    if (noPossibleLocation) {
+      return false;
+    } else if (singlePossibleLocation) {
+      const valuesAssignment = assignValues(possiblesMap, places[0], value);
+      if (failedRecursively(valuesAssignment)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+function eliminateRecursively(possiblesMap, cell, value) {
+  opCounts.eliminations++;
+
+  const previouslyEliminated = !possiblesMap[cell].includes(value);
+  if (previouslyEliminated) {
+    return possiblesMap;
+  }
+
+  possiblesMap[cell] = removeValue(possiblesMap[cell], value);
+
+  const constraints = [
+    propagateSingleValueConstraint(possiblesMap, cell),
+    propagateSingleLocationConstraint(possiblesMap, cell, value)
+  ];
+
+  const wereFailures = constraints.some(failedRecursively);
+  if (wereFailures) {
+    return false;
+  }
+
+  return possiblesMap;
+}
+
+function solveRecursivelyWithSearch(values) {
+  opCounts.searches++;
+
+  const failedInLastRecursiveCall = !values;
+  if (failedInLastRecursiveCall) {
+    return false;
+  }
+
+  const unsolvedCells = CELLS.filter(c => !isKnownValue(values[c]));
+
+  const puzzleIsSolved = unsolvedCells.length === 0;
+  if (puzzleIsSolved) {
+    return values;
+  }
+
+  const getCellWithFewestPossibles = (a, b) =>
+    values[a].length > values[b].length ? b : a;
+
+  const nextCell = unsolvedCells.reduce(getCellWithFewestPossibles);
+  const possibles = values[nextCell];
+
+  for (const p of possibles) {
+    const valuesAssignment = assignValues({ ...values }, nextCell, p);
+    const search = solveRecursivelyWithSearch(valuesAssignment);
+    const wasSuccessful = !failedRecursively(search);
+
+    if (wasSuccessful) {
+      return search;
+    }
+  }
+
+  return false;
+}
+
+const convertToValuesArray = valuesMap => {
+  return CELLS.map(c => parseInt(valuesMap[c], 10));
+};
+
+export const solvePuzzle = (gridString, trace = false) => {
+  const initialValues = calcPossibleValues(gridString);
+  const resultMap = solveRecursivelyWithSearch(initialValues);
+  const resultArray = convertToValuesArray(resultMap);
+  if (trace) {
+    console.log(
+      `Complexity: ${opCounts.assigns} assignments, ${opCounts.eliminations} eliminations, ${opCounts.searches} searches`
+    );
+    console.log('Result', resultArray);
+  }
+  return resultArray;
+};
