70b277a9418c9e9e3365cae42a2c962e2e293dc1
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-15 12:21:12 -0400

    Add paused message

--- src/components/GridOverlay.js
+++ src/components/GridOverlay.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { addAlpha } from 'styles/helpers';
+import Button from 'components/Button';
 
 const Styles = styled.div`
   grid-row: top / bottom;
   grid-column: start / end;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   background: ${p => addAlpha(p.theme.colors.background, 0.9)};
-  /* position: absolute; */
-  /* width: 100%;
-  height: 100%; */
   z-index: 500;
+  pointer-events: none;
+  opacity: 0;
+  transition: opacity 200ms;
+  ${p =>
+    p.show &&
+    css`
+      pointer-events: auto;
+      opacity: 1;
+    `}
 `;
 
-export default function GridOverlay() {
-  return <Styles></Styles>;
+const Message = styled.div`
+  font-weight: 700;
+  font-size: 3em;
+`;
+
+const Controls = styled.div``;
+
+const PausedMessage = ({ resume }) => (
+  <>
+    <Message>Game Paused</Message>
+    <Controls>
+      <Button onClick={resume}>Resume</Button>
+    </Controls>
+  </>
+);
+
+export default function GridOverlay({ state, actions }) {
+  const show = state.isPaused;
+
+  return (
+    <Styles show={show}>
+      {state.isPaused && <PausedMessage resume={actions.togglePaused} />}
+    </Styles>
+  );
 }
