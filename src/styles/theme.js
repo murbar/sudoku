f6062f51fb1cc5c3e340eb0455c496d3c151430c
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-23 21:48:02 -0400

    Add colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,20 +3,21 @@ import { media } from './helpers';
 const colors = {
   offWhite: '#fefaf0',
   offBlack: '#222',
-  // green: '#3cc473',
+  green: '#3cc473',
   cream: 'hsl(42, 45%, 90%)',
   darkCream: 'hsl(41, 45%, 84%)',
   cellHighlight: 'hsla(41, 45%, 84%, 0.5)',
   pine: 'hsl(167, 30%, 29%)',
   seaFoam: 'hsl(137, 78%, 88%)',
-  darkBrown: 'hsl(23, 10%, 26%)'
+  darkBrown: 'hsl(23, 10%, 26%)',
+  blue: 'steel blue'
 };
 
 const theme = {
   colors: {
     ...colors,
-    background: colors.offWhite,
-    foreground: colors.offBlack,
+    background: colors.cream,
+    foreground: colors.darkBrown,
     primary: colors.green
   },
   inputBorderRadius: `0.5rem`,
