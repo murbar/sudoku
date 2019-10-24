d6a854914a18f0f0d84319ffe93063ee46ef91ce
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-23 21:47:17 -0400

    Edit styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,9 @@ export default styled.button`
   font-size: 1em;
   font-weight: bold;
   border: none;
-  padding: 0.5rem 1rem;
+  padding: 0.75rem 1.5rem;
   background: rgba(0, 0, 0, 0.15);
-  border-radius: 0.5rem;
+  border-radius: 0.25rem;
   margin: 1rem 1rem 1rem 0;
   &:last-child {
     margin-right: 0;
