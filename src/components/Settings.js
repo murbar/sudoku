a36da577ac33ee77a7eb8cb3a5b4edc1f46d6ec7
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-27 13:46:06 -0500

    Add placeholders

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -7,6 +7,12 @@ export default function Settings() {
   return (
     <Styles>
       <h2>Settings</h2>
+      <div>Highlight focused regions</div>
+      <div>Highlight cells with same value</div>
+      <div>Highlight invalid entries</div>
+      <div>Sound effects on/off</div>
+      <div>Show/hide timer</div>
+      <div>Difficulty for new games</div>
     </Styles>
   );
 }
